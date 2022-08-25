ba6105d9dd574da630c45c76310fd4bfd7d77c92
kira <61452466+akshay-agney@users.noreply.github.com>
2022-08-25 17:14:54 +0530

    third congig.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_ofiy9yxUF', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6p3odpa8iip5a8kq6d5r5eapgv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_8AvtWSaSN', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '201k9fee03dfsv4rg9h67rm182', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
