daf7f1d768a1bb81699d21a6b936efa1db856600
kira <61452466+akshay-agney@users.noreply.github.com>
2022-08-25 15:50:40 +0530

    second update js/config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_OsSbTREpJ', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1122o067qmusm4pv16rdl1v615', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_ofiy9yxUF', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6p3odpa8iip5a8kq6d5r5eapgv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
